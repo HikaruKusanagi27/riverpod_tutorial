5e65a1dc02792dac89cc314ee8938e64051d9aff
hikarukusanagi <hikarugp24@gmail.com>
2025-08-02 21:06:43 +0900

    no message

---
+++ lib/statefull_widget_folder/counter_statefull.page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CounterStatefullPage extends StatefulWidget {
+  const CounterStatefullPage({super.key});
+
+  @override
+  State<CounterStatefullPage> createState() => _CounterStatefullPageState();
+}
+
+class _CounterStatefullPageState extends State<CounterStatefullPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
