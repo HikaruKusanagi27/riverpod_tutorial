6147218473151dd00872d8fea8d29723f53cf827
hikarukusanagi <hikarugp24@gmail.com>
2025-08-30 23:47:51 +0900

    no message

---
+++ lib/basics/studying_ui/ui_page.dart
@@ -0,0 +1,13 @@
+// フロントエンドのコンポーネント設計に立ち向かう
+// https://qiita.com/seya/items/8814e905693f00cdade2#%E3%82%B9%E3%82%BF%E3%82%A4%E3%83%AB%E3%82%AF%E3%83%AD%E3%83%BC%E3%82%BA%E3%83%89%E3%81%AE%E5%8E%9F%E5%89%87
+
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
