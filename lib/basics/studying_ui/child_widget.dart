fc2af9eb45021b932d1b8ce811d4f8972b1244ba
hikarukusanagi <hikarugp24@gmail.com>
2025-09-13 22:03:25 +0900

    no message

--- lib/basics/studying_ui/child_widget.dart
+++ lib/basics/studying_ui/child_widget.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class ChildWidget extends StatelessWidget {
+  const ChildWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/basics/studying_ui/parent_widget.dart
+++ lib/basics/studying_ui/parent_widget.dart
@@ -2,6 +2,7 @@
 // https://qiita.com/seya/items/8814e905693f00cdade2#%E3%82%B9%E3%82%BF%E3%82%A4%E3%83%AB%E3%82%AF%E3%83%AD%E3%83%BC%E3%82%BA%E3%83%89%E3%81%AE%E5%8E%9F%E5%89%87
 
 import 'package:flutter/material.dart';
+import 'package:riverpod_tutorial/basics/studying_ui/child_widget.dart';
 
 class ParentWidget extends StatelessWidget {
   const ParentWidget({super.key});
@@ -16,6 +17,7 @@ class ParentWidget extends StatelessWidget {
               '親Widget画面',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            ChildWidget(),
           ],
         ),
       ),
